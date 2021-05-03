14a59222720b491c6e1b3691c300138673c2f5a3
Dead_Light <amirrezawt@gmail.com>
2021-05-04 01:24:00 +0400

    first commit

---
+++ src/main/java/me/deadlight/bungeebalancerbukkit/onLogin.java
@@ -0,0 +1,103 @@
+package me.deadlight.bungeebalancerbukkit;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import fr.xephi.authme.events.LoginEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class onLogin implements Listener {
+
+    Random random = new Random();
+
+    //lollolooo player login shod hala azinja jer mikhorim
+    @EventHandler
+    public void onPlayerLogin(LoginEvent event) {
+
+        sendPlayerToLobbys(event.getPlayer());
+
+    }
+
+
+    private void sendPlayerToLobbys(Player player) {
+
+        String[] list = {"Lobby1", "Lobby2", "Lobby3"};
+
+
+        ArrayList<String> pservers = new ArrayList<>();
+
+        for (String sname : list) {
+            if (checkIfOnline(sname)) {
+                pservers.add(sname);
+            }
+        }
+
+
+        if (pservers.size() != 0) {
+            int rand = random.nextInt(pservers.size());
+            String server = list[rand];
+
+
+
+            final ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Connect");
+            out.writeUTF(server);
+            System.out.println("Sending " + player.getName() + " to the " + server);
+            player.sendPluginMessage(BungeeBalancerBukkit.getPlugin(), "BungeeCord", out.toByteArray());
+
+
+
+        } else {
+            player.sendMessage(utils.color("&cServer haye lobby dar hale tamir mibashad, Lofan kami sabr konid."));
+        }
+
+
+
+
+
+    }
+
+    private boolean checkIfOnline(String server) {
+        boolean online = false;
+        int port = getThePort(server);
+        try {
+            Socket s = new Socket();
+            s.connect(new InetSocketAddress("127.0.0.1", port), 20);
+            // ONLINE
+            s.close();
+            online = true;
+        } catch (UnknownHostException e) {
+            // OFFLINE
+        } catch (IOException e) {
+            // OFFLINE
+        }
+
+        return online;
+    }
+
+    private int getThePort(String servername) {
+
+        if (servername.equalsIgnoreCase("Lobby1")) {
+            return 27720;
+        } else if (servername.equalsIgnoreCase("Lobby2")) {
+            return 27722;
+        } else if (servername.equalsIgnoreCase("Lobby3")) {
+            return 27723;
+        } else {
+            return 27710;
+        }
+
+    }
+
+
+
+
+}
---
+++ src/main/java/me/deadlight/bungeebalancerbukkit/utils.java
@@ -0,0 +1,10 @@
+package me.deadlight.bungeebalancerbukkit;
+
+import org.bukkit.ChatColor;
+
+public class utils {
+
+    public static String color(String str) {
+        return ChatColor.translateAlternateColorCodes('&', str);
+    }
+}
